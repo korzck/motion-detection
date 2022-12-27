3b50828a4ad9c47342cacad569d32b9fe415d0f7
korzck <konstantin.koretskiy@gmail.com>
2022-12-27 23:51:47 +0300

    different image process

--- camera_input.cpp
+++ camera_input.cpp
@@ -1,8 +1,20 @@
-#include <opencv2/opencv.hpp> 
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
 using namespace cv;
-
-int main() 
+int main(int argc, char** argv)
 {
-    Mat image = imread("image.jpeg"); 
-    return 0; 
+	if (argc != 2) {
+		printf("usage: DisplayImage.out <Image_Path>\n");
+		return -1;
+	}
+	Mat image;
+	image = imread(argv[1], 1);
+	if (!image.data) {
+		printf("No image data \n");
+		return -1;
+	}
+	namedWindow("Display Image", WINDOW_AUTOSIZE);
+	imshow("Display Image", image);
+	waitKey(0);
+	return 0;
 }
