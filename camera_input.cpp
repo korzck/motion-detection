895576b59b0d01d386b283a8f68a1559fb3390c2
korzck <konstantin.koretskiy@gmail.com>
2022-12-30 23:47:46 +0300

    fixed compiling

--- camera_input.cpp
+++ camera_input.cpp
@@ -1,20 +1,15 @@
 #include <opencv2/opencv.hpp>
-#include <stdio.h>
+#include <iostream>
 using namespace cv;
+using namespace std;
 int main(int argc, char** argv)
 {
-	if (argc != 2) {
-		printf("usage: DisplayImage.out <Image_Path>\n");
-		return -1;
-	}
-	Mat image;
-	image = imread(argv[1], 1);
-	if (!image.data) {
-		printf("No image data \n");
-		return -1;
-	}
-	namedWindow("Display Image", WINDOW_AUTOSIZE);
-	imshow("Display Image", image);
-	waitKey(0);
-	return 0;
+    VideoCapture cap = VideoCapture(0);
+    OutputArray frame = {0};
+    for (int i = 0; ; i++)
+    {
+        if(!cap.read())
+            cout << "Streaming ended" << endl;
+
+    }
 }
