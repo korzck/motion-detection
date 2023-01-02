8a395a7d867c55d7705f4bf8cc2b4ddaff5aed05
korzck <konstantin.koretskiy@gmail.com>
2023-01-02 23:48:50 +0300

    added basic image process

--- frame_process.cpp
+++ frame_process.cpp
@@ -9,23 +9,15 @@ int main()
     Mat image = imread("image.jpeg");
 
 
-    Mat kernel1 = (Mat_<double>(3,3) << 0, 0, 0, 0, 1, 0, 0, 0, 0);
+    Mat kernel1 = (Mat_<double>(3,3) << -1, -1, -1, 0, 0, 0, 1, 1, 1);
     Mat identity; 
-    filter2D(image, identity, -1 , kernel1, Point(-1, -1), 0, 4);
+    filter2D(image, identity, -1 , kernel1, Point(-1, -1), 0, BORDER_DEFAULT);
     imshow("Original", image);
-    imshow("Identity", identity);
+    imshow("Filtered", identity);
     waitKey();
     imwrite("identity.jpg", identity);
     destroyAllWindows();
 
-    Mat kernel2 = Mat::ones(5,5, CV_64F);
-
-    kernel2 = kernel2 / 25;
-    Mat img;
-    filter2D(image, img, -1 , kernel2, Point(-1, -1), 0, 4);
-    imshow("Original", image);
-    imshow("Kernel blur", img);
-    imwrite("blur_kernel.jpg", img);
     waitKey();
     destroyAllWindows();
 }
