e8f8bed099b913c0dcae41b13fafad5cc2bedbf8
korzck <konstantin.koretskiy@gmail.com>
2023-01-02 23:40:55 +0300

    added basic image process

--- frame_process.cpp
+++ frame_process.cpp
@@ -1,34 +1,31 @@
 #include <opencv2/opencv.hpp>
-#include <stdio.h>
-using namespace cv;
+#include <iostream>
+
 using namespace std;
-int main(int argc, char** argv)
+using namespace cv;
+
+int main()
 {
-	Mat image, dst;
-	image = imread("image.jpeg", 1);
-    Mat kernel;
-    Point anchor;
-    double delta;
-    int ddepth;
-    int kernel_size;
-    anchor = Point( -1, -1 );
-    delta = 0;
-    ddepth = -1;
-    // Loop - Will filter the image with different kernel sizes each 0.5 seconds
-    int ind = 0;
-    for(;;)
-    {
-        // Update kernel size for a normalized box filter
-        kernel_size = 3 + 2*( ind%2 );
-        kernel = Mat::ones( kernel_size, kernel_size, CV_32F )/ (float)(kernel_size*kernel_size);
-        // Apply filter
-        filter2D(image, dst, ddepth , kernel, anchor, delta, BORDER_DEFAULT );
-        imshow("main", dst );
-        char c = (char)waitKey(500);
-        // Press 'ESC' to exit the program
-        if( c == 27 )
-        { break; }
-        ind++;
-    }
-	return 0;
+    Mat image = imread("image.jpeg");
+
+
+    Mat kernel1 = (Mat_<double>(3,3) << 0, 0, 0, 0, 1, 0, 0, 0, 0);
+    Mat identity; 
+    filter2D(image, identity, -1 , kernel1, Point(-1, -1), 0, 4);
+    imshow("Original", image);
+    imshow("Identity", identity);
+    waitKey();
+    imwrite("identity.jpg", identity);
+    destroyAllWindows();
+
+    Mat kernel2 = Mat::ones(5,5, CV_64F);
+
+    kernel2 = kernel2 / 25;
+    Mat img;
+    filter2D(image, img, -1 , kernel2, Point(-1, -1), 0, 4);
+    imshow("Original", image);
+    imshow("Kernel blur", img);
+    imwrite("blur_kernel.jpg", img);
+    waitKey();
+    destroyAllWindows();
 }
