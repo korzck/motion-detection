f1f333722b63e857c534794dde6ba7909e9510f9
korzck <konstantin.koretskiy@gmail.com>
2023-01-02 23:59:42 +0300

    added frame process

--- camera_input.cpp
+++ camera_input.cpp
@@ -1,20 +1,23 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include "frame_process.cpp"
 using namespace cv;
 using namespace std;
 int main(int argc, char** argv)
 {
-    namedWindow("main");
     VideoCapture cap(0);
     
     if (!cap.isOpened())
         cout << "cannot open camera" << endl;
     Mat frame;
-    
+    namedWindow("original");
     while(true)
     {
         cap >> frame;
-        imshow("main", frame);
-        waitKey(100);
+        imshow("original", frame);
+        // filter(frame);
+        // imshow("filtered", frame);
+        
     }
+    destroyAllWindows();
 }
--- frame_process.cpp
+++ frame_process.cpp
@@ -4,20 +4,18 @@
 using namespace std;
 using namespace cv;
 
-int main()
+void filter(Mat& image)
 {
-    Mat image = imread("image.jpeg");
-
-
+    // Mat image = imread("image.jpeg");
     Mat kernel1 = (Mat_<double>(3,3) << -1, -1, -1, 0, 0, 0, 1, 1, 1);
     Mat identity; 
     filter2D(image, identity, -1 , kernel1, Point(-1, -1), 0, BORDER_DEFAULT);
-    imshow("Original", image);
-    imshow("Filtered", identity);
-    waitKey();
-    imwrite("identity.jpg", identity);
-    destroyAllWindows();
+    // imshow("Original", image);
+    // imshow("Filtered", identity);
+    // waitKey();
+    // imwrite("identity.jpg", identity);
+    // destroyAllWindows();
 
-    waitKey();
-    destroyAllWindows();
+    // waitKey();
+    // destroyAllWindows();
 }
